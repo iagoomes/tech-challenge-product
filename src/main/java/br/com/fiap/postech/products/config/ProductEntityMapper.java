8da993e9f6929e01dd6226905d15a612e09c2436
Iago Gomes <iagoomes@outlook.com>
2025-02-14 16:18:57 -0300

    feat(batch): implement product batch upload functionality and related components
    - Added ProductBatchGatewayImpl to handle batch job execution
    - Configured JobLauncher and Job injection
    - Included logging for job start and error handling
    - Added warning log for missing Clean Architecture

--- src/main/java/br/com/fiap/postech/products/config/ProductEntityMapper.java
+++ src/main/java/br/com/fiap/postech/products/config/ProductEntityMapper.java
@@ -1,16 +1,15 @@
 package br.com.fiap.postech.products.config;
 
 import br.com.fiap.postech.products.infrastructure.persistence.ProductEntity;
+import io.micrometer.common.lang.NonNull;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
-import org.springframework.lang.NonNullApi;
-import org.springframework.validation.BindException;
 
 import java.math.BigDecimal;
 
 public class ProductEntityMapper implements FieldSetMapper<ProductEntity> {
-    @NonNullApi
-    public ProductEntity mapFieldSet(FieldSet fieldSet) throws BindException {
+    @Override
+    public @NonNull ProductEntity mapFieldSet(@NonNull FieldSet fieldSet) {
         ProductEntity productEntity = new ProductEntity();
         productEntity.setName(fieldSet.readString("name"));
         productEntity.setDescription(fieldSet.readString("description"));
